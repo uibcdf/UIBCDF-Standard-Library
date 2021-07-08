6bc54d484d90def6429368d5b5e8095b99022159
Diego Prada <prada.gracia@gmail.com>
2021-07-08 13:52:00 -0500

    update of the files structure in devtools/conda-envs

--- uibcdf_stdlib/input_arguments.py
+++ uibcdf_stdlib/input_arguments.py
@@ -2,7 +2,8 @@ import pyunitwizard as puw
 import numpy as np
 from .lists_and_tuples import is_list_or_tuple
 
-def check_input_argument(argument, argument_type, shape=None, dimensionality=None, value_type=None):
+def check_input_argument(argument, argument_type, shape=None, dtype_name=None, dimensionality=None,
+                         value_type=None, unit=None):
 
     if argument is 'ndarray':
         argument=np.ndarray
@@ -13,7 +14,7 @@ def check_input_argument(argument, argument_type, shape=None, dimensionality=Non
 
         for aux_argument_type in argument_type:
             aux_output = _check_single_input_argument(argument, aux_argument_type, shape=shape,
-                    dimensionality=dimensionality, value_type=value_type)
+                    dtype_name=dtype_name, dimensionality=dimensionality, value_type=value_type, unit=unit)
             if aux_output:
                 output=True
                 break
@@ -21,15 +22,17 @@ def check_input_argument(argument, argument_type, shape=None, dimensionality=Non
     else:
 
         output = _check_single_input_argument(argument, aux_argument_type, shape=shape,
-                dimensionality=dimensionality, value_type=value_type)
+                dtype_name=dtype_name, dimensionality=dimensionality, value_type=value_type, unit=unit)
 
     return output
 
-def _check_single_input_argument(argument, argument_type, shape=None, dimensionality=None, value_type=None):
+def _check_single_input_argument(argument, argument_type, shape=None, dtype_name=None,
+                                 dimensionality=None, value_type=None, unit=None):
 
     if argument_type is 'quantity':
 
-        output = local_puw.check(argument, dimensionality=dimensionality, value_type=value_type, shape=shape)
+        output = local_puw.check(argument, dimensionality=dimensionality, value_type=value_type,
+                                 shape=shape, dtype_name=dtype_name, unit=unit)
 
     else:
 
@@ -38,6 +41,14 @@ def _check_single_input_argument(argument, argument_type, shape=None, dimensiona
         if output:
             if shape is not None:
                 output = (np.shape(argument)==tuple(shape))
+
+        if output:
+            if dtype_name is not None:
+                try:
+                    aux_dtype_name=argument.dtype.name
+                    output = (aux_dtype_name==dtype_name)
+                except:
+                    output = False
 
     return output
 
---
+++ uibcdf_stdlib/tests/test_input_arguments.py
@@ -0,0 +1,46 @@
+import pytest
+import numpy as np
+from uibcdf_stdlib.inputs_arguments import check_input_argument
+import pyunitwizard as puw
+puw.configure.load_library('pint')
+
+def test_1():
+    argument = 5.0
+    output = check_input_argument(argument, float)
+    assert output == True
+
+def test_2():
+    argument = 5.0
+    output = check_input_argument(argument, str)
+    assert output == False
+
+def test_2():
+    argument = np.zeros([3,4])
+    output = check_input_argument(argument, np.ndarray, shape=(3,4))
+    assert output == True
+
+def test_3():
+    argument = puw.quantity(10, 'nm')
+    output = check_input_argument(argument, 'quantity', dimensionality={'[L]':1}, value_type=int)
+    assert output == True
+
+def test_4():
+    argument = puw.quantity(5.0, 'ps')
+    output = check_input_argument(argument, 'quantity', dimensionality={'[L]':1}, value_type=float)
+    assert output == False
+
+def test_5():
+    argument = puw.quantity([0,0,0], 'nm/ps')
+    output = check_input_argument(argument, 'quantity', dimensionality={'[L]':1, '[T]':-1}, value_type=list, shape=(3,))
+    assert output == True
+
+def test_6():
+    argument = puw.quantity(np.array([0,0,0]), 'ps')
+    output = check_input_argument(argument, 'quantity', value_type=np.ndarray, shape=(3,), unit='ps')
+    assert output == True
+
+def test_7():
+    argument = puw.quantity(np.array([0,0,0]), 'ps')
+    output = check_input_argument(argument, 'quantity', shape=(3,), unit='ns')
+    assert output == True
+
